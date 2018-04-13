38854146c41ee16381c53b767ace61674c630194
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2018-04-13 15:37:35 -0300

    Heap 1.0

--- src/Ej3/Heap.java
+++ src/Ej3/Heap.java
@@ -2,6 +2,7 @@ package Ej3;
 
 public class Heap<T>{
     Object[] elements = new Object[20];
+    double
 
     void enqueue(T elem,double priority){
 
@@ -10,6 +11,7 @@ public class Heap<T>{
     private class Node{
         T elem;
         double priority;
+        Node father, left, right;
 
         public Node(T elem, double priority) {
             this.elem = elem;
